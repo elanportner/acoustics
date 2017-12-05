c4e9f96f28687db54a0fe1ec8baacbf75da8f68b
Elan Portner <eportner@stanford.edu>
2017-12-05 14:39:45 -0800

    spatial dist of acoustic data

---
+++ Create_netCDF_with_acoustic_data.R
@@ -0,0 +1,49 @@
+
+# create and write the netCDF file -- ncdf4 version
+library(ncdf4)
+
+#read data, this has already been subsetted to only be nighttime 
+acoustics <- read.csv("~/Documents/R/Dosidicus/acoustics/acousticdata_uper200m_with_regions.csv")
+
+# define dimensions
+londim <- ncdim_def("lon","degrees_east",as.double(lon3)) 
+latdim <- ncdim_def("lat","degrees_north",as.double(lat3)) 
+timedim <- ncdim_def("time",tunits3,as.double(t3))
+
+# define variables
+fillvalue <- 1e32
+dlname <- "2m air temperature"
+tmp_def <- ncvar_def("tmp","deg_C",list(londim,latdim,timedim),fillvalue,dlname,prec="single")
+dlname <- "mean_temperture_warmest_month"
+mtwa_def <- ncvar_def("mtwa","deg_C",list(londim,latdim),fillvalue,dlname,prec="single")
+dlname <- "mean_temperature_coldest_month"
+mtco_def <- ncvar_def("mtco","deg_C",list(londim,latdim),fillvalue,dlname,prec="single")
+dlname <- "mean_annual_temperature"
+mat_def <- ncvar_def("mat","deg_C",list(londim,latdim),fillvalue,dlname,prec="single")
+
+# create netCDF file and put arrays
+ncfname <- "cru10min30_ncdf4.nc"
+ncout <- nc_create(ncfname,list(tmp_def,mtco_def,mtwa_def,mat_def),force_v4=T)
+
+# put variables
+ncvar_put(ncout,tmp_def,tmp_array3)
+ncvar_put(ncout,mtwa_def,mtwa_array3)
+ncvar_put(ncout,mtco_def,mtco_array3)
+ncvar_put(ncout,mat_def,mat_array3)
+
+# put additional attributes into dimension and data variables
+ncatt_put(ncout,"lon","axis","X") #,verbose=FALSE) #,definemode=FALSE)
+ncatt_put(ncout,"lat","axis","Y")
+ncatt_put(ncout,"time","axis","T")
+
+# add global attributes
+ncatt_put(ncout,0,"title",title$value)
+ncatt_put(ncout,0,"institution",institution$value)
+ncatt_put(ncout,0,"source",datasource$value)
+ncatt_put(ncout,0,"references",references$value)
+history <- paste("P.J. Bartlein", date(), sep=", ")
+ncatt_put(ncout,0,"history",history)
+ncatt_put(ncout,0,"Conventions",Conventions$value)
+
+# close the file, writing data to disk
+nc_close(ncout)
